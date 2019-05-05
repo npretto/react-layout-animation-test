fe15c65b5e28aec54a595f1dd0fc12444ed5eb63
npretto <info@npretto.com>
2019-05-05 12:16:40 +0200

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Header from "./components/Header/Header"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = { scrolled: false }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll)
+  }
+
+  handleScroll = e => {
+    const scrolled = window.pageYOffset > 300
+    if (scrolled != this.state.scrolled) this.setState({ scrolled })
+  }
+
+  render() {
+    const { scrolled } = this.state
+    return (
+      <div className="App">
+        <Header />
+        <img
+          className="background-image"
+          alt=""
+          src="https://s3-eu-west-1.amazonaws.com/production-hairdressr/fe-dummy/cover.png"
+        />
+        <div className="container">
+          <div
+            className={
+              "container--rounded-box" +
+              (scrolled ? " container--rounded-box--scrolled" : "")
+            }
+          />
+          <div className="container--content">
+            <p> Lorem ipsum dolor sit amet</p>
+            <h1>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam
+            </h1>
+            Lorem ipsum dolor sit amen
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+            <p>Lorem ipsum dolor sit amen</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <header className="header">
+      <p className="header-title">Astrubale</p>
+      <div>
+        <div />
+        <div>LOREM IPSUM</div>
+        <div>DOLOR</div>
+      </div>
+      <div />
+    </header>
+  )
+}
+
+export default Header
