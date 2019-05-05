83a7253a18946833d9715f0b85a19048ca400edb
npretto <info@npretto.com>
2019-05-05 17:58:30 +0200

    added post component

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,11 +1,31 @@
 import React from "react"
+import { ReactComponent as FlameIcon } from "../Icons/FlameIcon.svg"
+import { ReactComponent as PaperIcon } from "../Icons/PaperIcon.svg"
+import { ReactComponent as WaterIcon } from "../Icons/WaterIcon.svg"
 
-const Post = ({ small, title, text }) => {
+import "./Post.css"
+
+const Post = ({ small, title, text, author }) => {
   return (
-    <div>
-      <small>{small}</small>
-      <h1>{title}</h1>
-      <p>{text}</p>
+    <div className="post">
+      <small className="post--small">{small}</small>
+      <h1 className="post--title">{title}</h1>
+      <p className="post--text">{text}</p>
+      <div className="post--info">
+        <div className="author">
+          <img
+            className="author--pic"
+            src={author.pic}
+            alt={`Immagine profilo di ${author.displayName}`}
+          />
+          <div className="author--name">{author.displayName}</div>
+        </div>
+        <div className="post--info--buttons">
+          <FlameIcon className="post--info--button" />
+          <PaperIcon className="post--info--button" />
+          <WaterIcon className="post--info--button" />
+        </div>
+      </div>
     </div>
   )
 }
